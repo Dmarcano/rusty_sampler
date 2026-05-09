6121ab1e04e6e552e389d32a7be1754a98eb849e
Diego Marcano <marcanp.diego@gmail.com>
2026-05-09 11:48:10 -0400

    move oscilator to folder

--- crates/sampler-core/src/audio/mod.rs
+++ crates/sampler-core/src/audio/mod.rs
@@ -4,7 +4,7 @@ mod sink;
 mod source;
 mod spec;
 
-pub use oscillator::SineOscillator;
+pub use oscillator::sine::SineOscillator;
 pub use render::render_source_to_sink;
 pub use sink::{AudioSink, WavFileSink};
 pub use source::SampleSource;
